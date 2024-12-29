b37ac4a189210d86b0119716573932a693589294
SamisData <148009124+SamlovesData@users.noreply.github.com>
2024-12-28 20:47:38 -0500

    Initial commit of Spring BootProject

---
+++ src/main/java/com/example/dataprivacy/DataPrivacySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataprivacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPrivacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPrivacySystemApplication.class, args);
+	}
+
+}
